c76b06dc7f0134175d0bb9b77fef94dc18d9257d
Cesar Galvis <cgalvisleon@gmail.com>
2022-05-06 12:28:19 -0500

    Backup

--- src/env/env.js
+++ src/env/env.js
@@ -1,14 +1,14 @@
 const environment = {
-  title: "Dploy",
-  company: "Dploy SAS",
-  address: "Calle 3 # 12B - 41, San Gil, Colombia",
-  mobile: "+57 (301) 621 9802",
+  title: "Title",
+  company: "Company Name",
+  address: "Address Street, City, Country",
+  mobile: "+1 (111) 111 1111",
   copyright: "© 2016-2020",
-  storage: "dploy-user",
+  storage: "storage",
   production: process.env.NODE_ENV === "production",
   app: process.env.APP || "josephine",
-  url: process.env.URL || "http://192.168.0.3:3000",
-  mapbox: process.env.MAPBOX_KEY || "pk.eyJ1Ijoic2VydmljaW9zIiwiYSI6ImNpdzFjZmcyazA4YmIybm8ycXR0dWxiY3EifQ.78sqPHHt3mYQ8TymakHetA"
+  url: process.env.URL || "http://127.0.0.1:3000",
+  mapbox: process.env.MAPBOX_KEY || ""
 };
 
 module.exports = environment;
